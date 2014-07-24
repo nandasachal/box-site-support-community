aafae5a8df8d47bf3f08e007eb675cde72f55a42
Peter Bai <peter@peterbai.com>
2014-07-23 22:25:20 -0700

    First commit

---
+++ notes.md
@@ -0,0 +1,2 @@
+* Adding and deleting forum topics (big blue buttons):
+https://support.zendesk.com/entries/24279711-Managing-community-content-in-the-Help-Center
---
+++ p1.zdassets.com/hc/theme_assets/88181/3416/popup.js
@@ -0,0 +1,4 @@
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
